4fe6e950cde3583990bc3b7570cfeee02d103b1f
惠清川的Mac <chuange@huiqingchuandeMacdeMacBook-Air.local>
2016-09-21 21:46:06 +0800

    添加工程文件

---
+++ AnimationSample/AnimationSample/PositionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PositionViewController.swift
+//  AnimationSample
+//
+//  Created by 惠清川的Mac on 16/9/21.
+//  Copyright © 2016年 惠清川的Mac. All rights reserved.
+//
+
+import UIKit
+
+class PositionViewController: UIViewController {
+
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1) { 
+            self.blueView.center.x = self.view.bounds.width - self.blueView.center.x
+            
+        }
+        
+        UIView.animate(withDuration: 1, delay: 0.5, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: {
+            self.redView.center.y = self.view.bounds.height - self.redView.center.y
+            
+            }, completion: nil)
+        UIView.animate(withDuration: 1, delay: 1, options: UIViewAnimationOptions(rawValue: UInt(0)), animations: { 
+            self.greenView.center.x = self.view.bounds.width - self.greenView.center.x
+            self.greenView.center.y = self.view.bounds.height - self.greenView.center.y
+            }, completion: nil)
+        
+    }
+
+}
